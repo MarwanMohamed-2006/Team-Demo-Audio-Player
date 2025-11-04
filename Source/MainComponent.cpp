15543131b9364e9b49855a5bec5de5b3e6df80e6
Marwan Mohamed <marwanawad.7597@gmail.com>
2025-11-04 09:55:49 +0200

    Bouns task 1

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -6,6 +6,7 @@ MainComponent::MainComponent()
 {
 
     addAndMakeVisible(player1);
+    addAndMakeVisible(player2);
     setSize(1000, 500);
     setAudioChannels(0, 2);
 }
@@ -18,21 +19,34 @@ MainComponent::~MainComponent()
 void MainComponent::prepareToPlay(int samplesPerBlockExpected, double sampleRate)
 {
     player1.prepareToPlay(samplesPerBlockExpected, sampleRate);
+    player2.prepareToPlay(samplesPerBlockExpected, sampleRate);
 }
 
 void MainComponent::getNextAudioBlock(const juce::AudioSourceChannelInfo& bufferToFill)
 {
+    bufferToFill.clearActiveBufferRegion();
     player1.getNextAudioBlock(bufferToFill);
+    juce::AudioBuffer<float> tempBuffer(bufferToFill.buffer->getNumChannels(),bufferToFill.numSamples);
+    juce::AudioSourceChannelInfo tempbuffer(&tempBuffer, 0, bufferToFill.numSamples);
+        
+    player2.getNextAudioBlock(tempbuffer);
+    for (int channel = 0; channel < bufferToFill.buffer->getNumChannels(); ++channel)
+    {
+        bufferToFill.buffer->addFrom(channel,bufferToFill.startSample,tempBuffer,channel,0,bufferToFill.numSamples);
+    }
 }
+
 
 void MainComponent::releaseResources()
 {
     player1.releaseResources();
+    player2.releaseResources();
 }
 
 
 void MainComponent::resized()
 {
-    player1.setBounds(getLocalBounds());
+    int halfHeight = getHeight() / 2;
+    player1.setBounds(0, 0, getWidth(), halfHeight);
+    player2.setBounds(0, halfHeight, getWidth(), getHeight() - halfHeight);
 }
-
